9d47b0334d0930ef94a98280743daa2d47ec6e2b
Grozea Tatiana <tallya@Grozeas-MacBook-Air.local>
2024-01-25 20:37:01 +0200

    initial commit

---
+++ code.js
@@ -0,0 +1,33 @@
+function handleYes() {
+    const header = document.querySelector("h1");
+    //console.log(header);
+    header.innerText = "Lezzz goo!";
+    const yes = document.getElementById("yes");
+    const no = document.getElementById("no");
+    yes.remove();
+    no.remove();
+    document.getElementById("img1").src =
+        "https://media1.tenor.com/m/nZuiRcnxoxsAAAAC/catusjksk.gif";
+}
+
+function handleNo() {
+    //console.log("w");
+    const no = document.getElementById("no");
+    const newTop = random();
+    const newLeft = random();
+    no.style.top = `${newTop}px`;
+    no.style.left = `${newLeft}px`;
+}
+
+function handleNoClick() {
+    const no = document.getElementById("no");
+    const newTop = random();
+    const newLeft = random();
+    no.style.top = `${newTop}px`;
+    no.style.left = `${newLeft}px`;
+    document.getElementById("img1").src = "https://media1.tenor.com/m/nZuiRcnxoxsAAAAC/catusjksk.gif";
+}
+
+function random() {
+    return Math.floor(Math.random() * 300);
+}
