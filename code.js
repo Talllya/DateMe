554e1751288bb40504ef675b5531ad6fcdc75098
Grozea Tatiana <taneagrozea2003@gmail.com>
2024-02-12 15:43:16 +0200

    background finally done

--- code.js
+++ code.js
@@ -1,7 +1,5 @@
 function handleYes() {
-    // new comment
     const header = document.querySelector("h1");
-    //console.log(header);
     header.innerText = "Lezzz goo!";
     const yes = document.getElementById("yes");
     const no = document.getElementById("no");
@@ -14,33 +12,20 @@ function handleYes() {
 }
 
 function handleNo() {
-    //console.log("w");
     const no = document.getElementById("no");
     const newTop = random(300);
     const newLeft = random(300);
     no.style.top = `${newTop}px`;
     no.style.left = `${newLeft}px`;
 
-    const img = document.getElementById("img");
+    /*const img = document.getElementById("img");
     img.src = "catNo.gif";
 
     const nwTop = random(1100);
     const nwLeft = random(1100);
     img.style.top = `${nwTop}px`;
     img.style.left = `${nwLeft}px`;
-      
-}
-
-function handleNoClick() {
-    const no = document.getElementById("no");
-    const newTop = random(300);
-    const newLeft = random(300);
-    document.getElementById("img1").src = "catNo.gif";
-    no.style.top = `${newTop}px`;
-    no.style.left = `${newLeft}px`;
-
-    const img = document.getElementById("img");
-    img.remove();
+    */
 }
 
 function random(size) {
